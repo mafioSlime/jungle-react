62c8f9666ff1d24a51e1c5c0c2f5f335bbf5e65e
John Paguirigan <johnjaroldxvi@gmail.com>
2020-03-18 18:03:41 -0600

    added header component

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="header">
+        <h1 className="header-h1">WELCOME TO THE JUNGLE</h1>
+        <a href="#" className="header-a">
+          EXPLORE
+        </a>
+      </header>
+    );
+  }
+}
+
+export default Header;
